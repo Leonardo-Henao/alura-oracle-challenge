53bf7cdbc8cd96d242a4bdf43f7533298f5adf39
Leonardo Henao <leonardofhenao.085@gmail.com>
2024-07-05 16:37:26 -0500

    Feat: Proyecto finalizado ⭐

---
+++ README.md
@@ -0,0 +1,32 @@
+# Challenge Alura + Oracle
+
+Challenge de encriptador y desencriptador creado por [Leonardo Henao](https://leonardohenao.com)
+
+## Instalación
+
+- Clone el repositorio de la rama `main`
+- Ejecute el servidor de confianza para abrir el proyecto en su navegador
+  - `Live server` si utiliza [Visual Studio Code](https://code.visualstudio.com/)
+  - `Light server` para ser ejecutado desde consola, recuerde instalar el paquete de npm [light server](https://www.npmjs.com/package/light-server)
+
+## Instrucciones
+
+Al escribir un texto y tocar al boton **Encriptar** el sistema sustituira cada letra(llave) por el valor de la llave correspondiente. _[Vea llaves de encriptacion y desencriptacion](/#llaves-de-encriptacion-y-desencriptacion)_
+
+Ejemplo:
+`"gato" => "gaitober"`
+
+Al escribir un texto y tocar al boton **Desencriptar** el sistema sustituira cada valor encontrado en el texto por la llave correspondiente.
+
+Ejemplo:
+`gaitober" => "gato"`
+
+## Llaves de encriptacion y desencriptacion
+
+| llave | valor |
+| ----- | ----- |
+| a     | ai    |
+| e     | enter |
+| i     | imes  |
+| o     | ober  |
+| u     | ufat  |
---
+++ script.js
@@ -0,0 +1,225 @@
+/**
+ * Get element by id
+ * @param [string] id id from HTML element
+ */
+const getById = (id) => document.getElementById(id);
+
+// GUI Elements
+const toast = getById('toast');
+const texAreaEntry = getById('text_to_convert');
+const sectionOutput = getById('section_output');
+const texAreaOutput = getById('text_from_convert');
+const contentNoData = getById('no_data');
+
+// Keys to encrypt and decrypt
+const keysEncryptDencrypt = [
+  { key: 'a', value: 'ai' },
+  { key: 'e', value: 'enter' },
+  { key: 'i', value: 'imes' },
+  { key: 'o', value: 'ober' },
+  { key: 'u', value: 'ufat' },
+];
+
+var showKeysTooltip = false;
+
+/**
+ * Handler to button encrypt
+ */
+const encryptHandler = () => {
+  if (!textAreaIsEmpty()) encrypt();
+  else showToast('Debes ingresar un texto para encriptar');
+};
+
+/**
+ * Handler to button decrypt
+ */
+const decryptHandler = () => {
+  if (!textAreaIsEmpty()) decrypt();
+  else showToast('Debes ingresar un texto para desencryptar');
+};
+
+/**
+ * Encrypt the text
+ */
+const encrypt = () => {
+  const value = getTextAreaValue();
+  const arrayValue = Array.from(value);
+
+  const result = arrayValue
+    .map((x) => {
+      const findCharacter = keysEncryptDencrypt.find((y) => y['key'] == x);
+      return findCharacter ? findCharacter['value'] : x;
+    })
+    .join('');
+
+  setResultToTextAreaOutput(result);
+  handlerViewImageNoData();
+};
+
+/**
+ * Decrypt the text
+ */
+const decrypt = () => {
+  let value = getTextAreaValue();
+  keysEncryptDencrypt.forEach((x) => (value = value.replace(new RegExp(x['value'], 'g'), x['key'])));
+
+  setResultToTextAreaOutput(value);
+  handlerViewImageNoData();
+};
+
+/**
+ * Set result to TexAreaOutput
+ * @param [string] text to show
+ */
+const setResultToTextAreaOutput = (result) => {
+  texAreaOutput.value = result;
+  if (showKeysTooltip) getById('btn_copy').appendChild(setKeyButton('C'));
+};
+
+/**
+ * Control view to TexAreaOutput and image when TextAreaOutput is empty
+ */
+const handlerViewImageNoData = () => {
+  const data = texAreaOutput.value.length;
+  if (data > 0) {
+    sectionOutput.style.display = 'flex';
+    contentNoData.style.display = 'none';
+  } else {
+    sectionOutput.style.display = 'none';
+    contentNoData.style.display = 'flex';
+  }
+};
+
+/**
+ * Clan all text in texAreas
+ */
+const cleanAllHandler = () => {
+  if (texAreaEntry.value.length == 0 && texAreaOutput.value.length == 0) {
+    showToast('No hay datos para limpiar');
+    return;
+  }
+  texAreaEntry.value = '';
+  texAreaOutput.value = '';
+  handlerViewImageNoData();
+};
+
+/**
+ * Copy to clipboard the text converted
+ */
+const copyTextHandler = () => {
+  const text = texAreaOutput.value;
+  const type = 'text/plain';
+
+  if (ClipboardItem.supports(type)) {
+    const blob = new Blob([text], { type });
+    const data = [new ClipboardItem({ [type]: blob })];
+    navigator.clipboard.write(data);
+    showToast('Texto copiado');
+  } else console.log('No soportado');
+};
+
+/**
+ * Show information text to user
+ */
+const showTextAlertHandler = () => showToast('Solo letras sin acentos');
+
+/**
+ * Get the text from texArea
+ * @returns [string] with text value
+ */
+const getTextAreaValue = () => texAreaEntry.value.toString().toLowerCase();
+
+/**
+ * Check if texArea is empty
+ * @returns [boolean] with value if empty or not
+ */
+const textAreaIsEmpty = () => !texAreaEntry.value.length > 0;
+
+/**
+ * Show a toast message
+ * @param message text to show
+ */
+const showToast = (message) => {
+  toast.setAttribute('show', true);
+  toast.innerHTML = message;
+
+  setTimeout(() => {
+    toast.setAttribute('show', false);
+  }, 5000);
+};
+
+/**
+ * Handler tooltip view on all buttons
+ */
+const buttonKeyTooltipHandler = () => {
+  const allTooltipKeys = Array.from(document.getElementsByClassName('key_button_tooltip'));
+  if (showKeysTooltip) {
+    showKeysTooltip = false;
+    allTooltipKeys.forEach((x) => x.remove());
+  } else {
+    showKeysTooltip = true;
+    appendChildTooltips();
+  }
+};
+
+/**
+ * Set key event listener
+ */
+document.addEventListener('keydown', (event) => {
+  const key = event.key.toLowerCase();
+  const textAreaFocused = texAreaEntry === document.activeElement;
+
+  if (textAreaFocused && key === 'escape') texAreaEntry.blur();
+  else if (!textAreaFocused) {
+    switch (key) {
+      case 'k':
+        buttonKeyTooltipHandler();
+        break;
+      case 'e':
+        encryptHandler();
+        break;
+      case 'd':
+        decryptHandler();
+        break;
+      case 'r':
+        cleanAllHandler();
+        break;
+      case 'c':
+        copyTextHandler();
+        break;
+      case 't':
+        texAreaEntry.focus();
+        break;
+      case 'escape':
+        if (showKeysTooltip) buttonKeyTooltipHandler();
+        break;
+    }
+  }
+});
+
+/**
+ * Create key tooltip
+ */
+const setKeyButton = (key) => {
+  const div = document.createElement('div');
+  div.setAttribute('class', 'key_button_tooltip');
+  div.innerHTML = key;
+  return div;
+};
+
+/**
+ * Add tooltip to all buttons
+ */
+const appendChildTooltips = () => {
+  try {
+    getById('section_entry').appendChild(setKeyButton('T: Entrar | Escape: Salir'));
+    getById('btn_encrypt').appendChild(setKeyButton('E'));
+    getById('btn_dencrypt').appendChild(setKeyButton('D'));
+    getById('btn_clean').appendChild(setKeyButton('R'));
+    getById('btn_copy').appendChild(setKeyButton('C'));
+  } catch {}
+};
+
+setTimeout(() => {
+  getById('transitions-anim').style.display = 'none';
+}, 6300);
